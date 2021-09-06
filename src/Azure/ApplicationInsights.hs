d1fc8a83a234e9639be3748e0ba729d1a4f58d44
Victor Nascimento Bakke <gipphe@gmail.com>
2021-09-06 23:51:53 +0200

    chore: more wip

--- src/Azure/ApplicationInsights.hs
+++ src/Azure/ApplicationInsights.hs
@@ -4,13 +4,6 @@ module Azure.ApplicationInsights where
 
 import Data.Text (Text)
 
-data TelemetryType
-    = EventData
-
-telemetryTypeToText :: TelemetryType -> Text
-telemetryTypeToText = \case
-    EventData -> "EventData"
-
 trackEvent :: Text -> EventPayload -> IO ()
 trackEvent eventName payload = do
     undefined
